df84471c42d0e9a6265a7b38fe6252cbbc876afb
kgb1001001 <kgb1001001@aol.com>
2015-09-03 11:26:53 -0400

    setup changed for validation

--- setupcf.sh
+++ setupcf.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
 # setup script
-if [ "$#" -ne 3 ]; then
-        echo "ERROR: setup takes 3 parameters"
-        echo "usage setup local_directory container_ip container_port"
+N="kgb1001001"
+if [ "$#" -ne 5 ]; then
+        echo "ERROR: setupcf takes 5 parameters"
+        echo "usage setup local_directory image_name container_name container_ip container_port"
         exit 1
 else
         echo "Phase 1->building container locally"
-        sudo docker build -t kbrown/$1 $1
+        sudo docker build -t $2 $1
         echo "Phase 2->tagging container"
-        sudo docker tag $1 \
-registry.ng.bluemix.net/kbrown/$1
+        sudo docker tag $2 \
+registry.ng.bluemix.net/$N/$2
         echo "Phase 3->pushing container to bluemix"
-        sudo docker push registry.ng.bluemix.net/kbrown/$1
+        sudo docker push registry.ng.bluemix.net/$N/$2
         echo "Phase 4->validating image list"
         sudo cf ic images | grep $1
         echo "Phase 5->running container on bluemix"
-        sleep 25s
-        sudo cf ic run --memory 2048 --name $1 -p $3 \
-registry.ng.bluemix.net/kbrown/$1:latest
+        sleep 45s
+        sudo cf ic run --name=$3 -publish=$5 \
+registry.ng.bluemix.net/$N/$1:latest
         echo "Phase 6->binding IP address"
-        sudo cf ic ip bind $1 $2
+        sudo cf ic ip bind $3 $4
         exit 0
 fi
